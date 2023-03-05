0273ea2365530c220f5773338d80d0b4cbf5a639
Milena Vargas Leonardi <milena.leonardi@hotmail.com>
2023-03-04 22:11:02 -0300

    Create banco-de-dados.sql

---
+++ banco-de-dados.sql
@@ -0,0 +1,10 @@
+#Tabela criada no MYSQL para armazenar dados de clientes
+
+CREATE TABLE Clientes (
+    CPF varchar(14),
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50)
+);
